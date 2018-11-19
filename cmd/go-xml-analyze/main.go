6af542873e73a3d1519d522d22db786f152db0b2
mike_neck <jkrt3333@gmail.com>
2018-11-20 01:56:29 +0900

    pom 解析のコードサンプル

---
+++ cmd/go-xml-analyze/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/xml"
+	"log"
+	"os"
+)
+
+func main() {
+	pom, err := os.Open("cmd/go-xml-analyze/testdata/junit-jupiter-api-5.0.0-M3.pom")
+	if err != nil {
+		log.Fatalln("failed to open pom file.", err)
+	}
+	defer pom.Close()
+
+	decoder := xml.NewDecoder(pom)
+	token, err := decoder.Token()
+	if err != nil {
+		log.Fatalln("failed to get token", err)
+	}
+
+	token, err = decoder.Token()
+	if err != nil {
+		log.Fatalln("failed to get token", err)
+	}
+
+	token, err = decoder.Token()
+	if err != nil {
+		log.Fatalln("failed to get token", err)
+	}
+
+	switch tokenType := token.(type) {
+	case xml.StartElement:
+		var project Model
+		err := project.UnmarshalXML(decoder, tokenType)
+		if err != nil {
+			log.Fatalln("failed to parse pom", err)
+		}
+		log.Println(project)
+		for _, dep := range project.Dependencies.Dependency {
+			log.Println(dep)
+		}
+	default:
+		log.Println("type of token", tokenType)
+	}
+}
